243d73388eb4e8e6e1d6abd9ba6d86a6ede9ad45
Bastian Feder <github@bastian-feder.de>
2011-07-07 11:22:18 +0200

    more tests for classbuilder

--- tests/Unit/classBuilderTest.php
+++ tests/Unit/classBuilderTest.php
@@ -41,6 +41,7 @@ namespace TheSeer\phpDox\Tests\Unit {
     use lapistano\ProxyObject\ProxyObject;
     use TheSeer\phpDox\Tests\phpDox_TestCase;
     use TheSeer\phpDox\ClassBuilder;
+    use TheSeer\fDOM\fDOMElement;
 
     class ClassBuilderTest extends phpDox_TestCase {
 
@@ -49,11 +50,47 @@ namespace TheSeer\phpDox\Tests\Unit {
         /* Fixtures                                                          */
         /*********************************************************************/
 
-        protected function getFDomElementFixture (array $methods) {
+        /**
+         * Provides as stub of the \TheSeer\fDOM\fDOMElement class.
+         *
+         * @param array $methods
+         * @return \TheSeer\fDOM\fDOMElement
+         */
+        protected function getFDomElementFixture(array $methods = array()) {
             return $this->getMockBuilder('\\TheSeer\\fDOM\\fDOMElement')
                 ->disableOriginalConstructor()
                 ->setMethods($methods)
                 ->getMock();
+        }
+
+        /**
+         * Provides a stub of the \TheSeer\phpDox\DocBlock\Parser class.
+         *
+         * @param array $methods
+         * @return \TheSeer\phpDox\DocBlock\Parser
+         */
+        protected function getParserFixture(array $methods = array()) {
+            return $this->getMockBuilder('\\TheSeer\\phpDox\\DocBlock\\Parser')
+                ->disableOriginalConstructor()
+                ->setMethods($methods)
+                ->getMock();
+        }
+
+        /**
+         * Provides an instance of the \TheSeer\phpDox\classBuilderProxy class
+         *
+         * @return \TheSeer\phpDox\classBuilderProxy
+         */
+        protected function getClassBuilderProxyFixture() {
+
+            $node = $this->getFDomElementFixture(array('setAttribute'));
+            $parser = $this->getParserFixture();
+            $ctx = $this->getFDomElementFixture(array());
+            $aliasMap = array();
+            $publicOnly = false;
+            $encoding = 'ISO-8859-1';
+
+            return new classBuilderProxy($parser, $ctx, $aliasMap, $publicOnly, $encoding);
         }
 
         /*********************************************************************/
@@ -61,40 +98,95 @@ namespace TheSeer\phpDox\Tests\Unit {
         /*********************************************************************/
 
         /**
+         * @dataProvider addReferenceNodeDataprovider
          * @covers \TheSeer\phpDox\ClassBuilder::addReferenceNode
          */
-        public function dtestAddReferenceNode() {
-            $nodeName = 'implemts'; // extends
+        public function testAddReferenceNode($nodeName, $inheritance, $classShortName, $classPath) {
+            $node = $this->getFDomElementFixture(array('setAttribute'));
+            $node
+                ->expects($this->once())
+                ->method('setAttribute')
+                ->with(
+                    $this->equalTo($inheritance),
+                    $this->equalTo($classShortName)
+                );
 
-            $dom = $this->getDomDocument();
-            $newNode = new \DOMElement($nodeName);
-            $dom->appendChild($newNode);
-            $node = $dom->getElementsByTagname($nodeName)->item(0);
-
-            $class = new \ReflectionClass('\\stdClass');
+            $class = new \ReflectionClass($classPath);
             $context = $this->getFDomElementFixture(array('appendElementNS'));
             $context
                 ->expects($this->once())
                 ->method('appendElementNS')
                 ->with(
-                    $this->logicalAnd(
-                        $this->equalTo('http://xml.phpdox.de/src#'),
-                        $this->equalTo('class')
-                    )
+                    $this->equalTo('http://xml.phpdox.de/src#'),
+                    $this->equalTo($nodeName)
                 )
                 ->will($this->returnValue($node));
 
-            $proxy = new ProxyObject();
-            $classBuilder = $proxy->getProxyBuilder('\\TheSeer\\phpDox\\ClassBuilder')
-                ->disableOriginalConstructor()
-                ->setMethods(array('addReferenceNode'))
-                ->getProxy();
+            $classBuilder = $this->getClassBuilderProxyFixture();
+            $this->assertInstanceOf('\DomElement', $classBuilder->addReferenceNode($class, $context, $nodeName));
+        }
 
-            $actualNode = $classBuilder->addReferenceNode($class, $context, $nodeName);
+        /**
+         * @dataProvider addReferenceNodeWithNamspacedClassDataprovider
+         * @covers \TheSeer\phpDox\ClassBuilder::addReferenceNode
+         */
+        public function testAddReferenceNodeWithNamspacedClass($nodeName, $inheritance, $classShortName, $classPath) {
+            $node = $this->getFDomElementFixture(array('setAttribute'));
+            $node
+                ->expects($this->exactly(2))
+                ->method('setAttribute')
+                ->with(
+                    $this->logicalOr(
+                        $this->equalTo($inheritance),
+                        $this->equalTo('namespace')
+                    ),
+                    $this->logicalOr(
+                        $this->equalTo($classShortName),
+                        $this->equalTo('TheSeer\phpDox')
+                    )
+                );
 
-            var_dump($actualNode);
+            $class = new \ReflectionClass($classPath);
+            $context = $this->getFDomElementFixture(array('appendElementNS'));
+            $context
+                ->expects($this->once())
+                ->method('appendElementNS')
+                ->with(
+                    $this->equalTo('http://xml.phpdox.de/src#'),
+                    $this->equalTo($nodeName)
+                )
+                ->will($this->returnValue($node));
 
-            $this->markTestIncomplete('Due to bug in proxy-object.');
+            $classBuilder = $this->getClassBuilderProxyFixture();
+            $this->assertInstanceOf('\DomElement', $classBuilder->addReferenceNode($class, $context, $nodeName));
+        }
+
+        /*********************************************************************/
+        /* Dataprovider & callbacks                                          */
+        /*********************************************************************/
+
+        public static function addReferenceNodeWithNamspacedClassDataprovider() {
+            return array(
+                'inheritance in namespace' =>
+                    array('extends', 'class', 'ClassBuilder', '\\TheSeer\\phpDox\\ClassBuilder'),
+                'implementation in namespace' =>
+                    array('implements', 'interface', 'ClassBuilder', '\\TheSeer\\phpDox\\ClassBuilder'),
+            );
+        }
+
+        public static function addReferenceNodeDataprovider() {
+            return array(
+                'inheritance' => array('extends', 'class', 'stdClass', '\\stdClass'),
+                'implementation' => array('implements', 'interface', 'stdClass', '\\stdClass'),
+            );
+        }
+
+    }
+
+    class classBuilderProxy extends \TheSeer\phpDox\ClassBuilder {
+
+        public function addReferenceNode(\ReflectionClass $class, fDOMElement $context, $nodeName) {
+            return parent::addReferenceNode($class, $context, $nodeName);
         }
     }
 }
