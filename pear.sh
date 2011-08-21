854e34c4aba89d29ae707ec7de6b91b166d5be2d
Arne Blankerts <Arne@Blankerts.de>
2011-08-21 15:54:11 +0200

    Adjust pear.sh and package.xml for new files and structure as well rebuild autoload

--- pear.sh
+++ pear.sh
@@ -6,10 +6,9 @@ cp -r templates/* tmp/TheSeer/phpDox/templates
 cp package.xml tmp
 cp phpdox.bat tmp
 cd tmp
-sed -e "s@require __DIR__ . '/src/@require 'TheSeer/phpDox/@" ../phpdox.php | \
-sed -e "s@require __DIR__ . '/lib/staticReflection/src/main/php/@require '@" > phpdox.php
+sed -e "s@require __DIR__ . '/src/@require 'TheSeer/phpDox/@" ../phpdox.php > phpdox.php
 
-sed -e "s@__DIR__ . '/../templates'@__DIR__ . '/templates'@" ../src/cli.php > TheSeer/phpDox/cli.php
+sed -e "s@__DIR__ . '/../templates'@__DIR__ . '/templates'@" ../src/CLI.php > TheSeer/phpDox/CLI.php
 pear package
 mv phpDox*.tgz ..
 cd ..
