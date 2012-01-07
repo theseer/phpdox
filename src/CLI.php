3bb10b35cbe395e4a16d08126aa85d5b5f6ddb2c
Arne Blankerts <Arne@Blankerts.de>
2012-01-07 21:35:02 +0100

    Bump Copyright year

--- src/CLI.php
+++ src/CLI.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) 2010-2011 Arne Blankerts <arne@blankerts.de>
+ * Copyright (c) 2010-2012 Arne Blankerts <arne@blankerts.de>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
@@ -174,7 +174,7 @@ namespace TheSeer\phpDox {
                 return;
             }
             $shown = true;
-            printf("phpdox %s - Copyright (C) 2010 - 2011 by Arne Blankerts\n\n", self::VERSION);
+            printf("phpdox %s - Copyright (C) 2010 - 2012 by Arne Blankerts\n\n", self::VERSION);
         }
 
         protected function showSkeletonConfig($strip) {
