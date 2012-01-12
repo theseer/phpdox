a9d0b759e995f844db58ab1b114bc4051dc73d79
Arne Blankerts <Arne@Blankerts.de>
2012-01-12 22:38:26 +0100

    Prepare 0.4 release

--- pear.sh
+++ pear.sh
@@ -3,14 +3,10 @@ rm -f phpDox*.tgz
 mkdir -p tmp/TheSeer/phpDox/templates
 cp -r src/* tmp/TheSeer/phpDox
 cp -r templates/* tmp/TheSeer/phpDox/templates
-cp package.xml tmp
 cp phpdox.bat tmp
 cd tmp
-sed -e "s@require __DIR__ . '/src/@require 'TheSeer/phpDox/@" ../phpdox.php > phpdox.php
-
-sed -e "s@__DIR__ . '/../templates'@__DIR__ . '/templates'@" ../src/CLI.php > TheSeer/phpDox/CLI.php
+php ../../DirectoryScanner/samples/pear-package.php ../package.xml . | xmllint --format - > package.xml
 pear package
 mv phpDox*.tgz ..
 cd ..
 rm -rf tmp
-
