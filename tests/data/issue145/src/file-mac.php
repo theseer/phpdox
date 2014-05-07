555ede9c6609c2775d978c88cf59e1ad63674f47
Arne Blankerts <Arne@Blankerts.de>
2014-05-08 01:49:05 +0200

    testcase for issue #145

---
+++ tests/data/issue145/src/file-mac.php
@@ -0,0 +1,1 @@
+<?php/** * Here be dragons. * Line 1 * Line 2 */interface MacFooInterface{}
---
+++ tests/data/issue145/src/file-unix.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Here be dragons.
+ * Line 1
+ * Line 2
+ */
+interface UnixFooInterface
+{
+}
+
