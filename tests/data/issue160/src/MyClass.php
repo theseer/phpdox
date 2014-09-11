f9cdd4645d1762d17c4483d848446b3f2177c670
Arne Blankerts <Arne@Blankerts.de>
2014-09-11 16:46:24 +0200

    Testcase for issue #160

---
+++ tests/data/issue160/src/MyClass.php
@@ -0,0 +1,13 @@
+<?php
+namespace {
+
+    /**
+     * Class MyClass
+     */
+    class MyClass {
+        /*
+         * bytes '0xA0 0x7B 0x40 0x69'
+         */
+        const X = ' {@i';
+    }
+}
