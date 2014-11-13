795351f5cc1eb14e2a183511a13553589f6efb5a
Arne Blankerts <Arne@Blankerts.de>
2014-11-13 18:27:04 +0100

    Added additional testcase for #160

---
+++ tests/data/issue160/src/OtherClass.php
@@ -0,0 +1,16 @@
+<?php
+namespace {
+
+    /**
+     * Class MyClass
+     */
+    class OtherClass {
+
+        /*
+         * bytes '0xA0 0x7B 0x40 0x69'
+         */
+        const value = '<?xml version="1.0" encoding="utf-8" ?><r>�{@i</r>';
+
+    }
+
+}
