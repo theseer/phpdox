d44b9ed8322650de59d12e8d1f1bf880e03770f1
Arne Blankerts <Arne@Blankerts.de>
2015-01-02 19:25:34 +0100

    Cleanup

--- src/collector/backend/parser/PHPParser.php
+++ src/collector/backend/parser/PHPParser.php
@@ -37,7 +37,7 @@
 namespace TheSeer\phpDox\Collector\Backend {
 
     use TheSeer\phpDox\Collector\SourceFile;
-    use \TheSeer\phpDox\DocBlock\Parser as DocblockParser;
+    use TheSeer\phpDox\DocBlock\Parser as DocblockParser;
 
     /**
      *
@@ -50,10 +50,13 @@ namespace TheSeer\phpDox\Collector\Backend {
         private $parser = NULL;
 
         /**
-         * @var \TheSeer\phpDox\DocBlock\Parser
+         * @var DocblockParser
          */
         private $docblockParser = NULL;
 
+        /**
+         * @param DocblockParser $parser
+         */
         public function __construct(DocblockParser $parser) {
             $this->docblockParser = $parser;
         }
