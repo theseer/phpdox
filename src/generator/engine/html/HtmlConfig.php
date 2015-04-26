1fff81e6eaeed6f97db4d404a4497d22a7d95dc0
Arne Blankerts <Arne@Blankerts.de>
2015-04-26 14:00:14 +0200

    Fix indentation

--- src/generator/engine/html/HtmlConfig.php
+++ src/generator/engine/html/HtmlConfig.php
@@ -34,14 +34,13 @@
  * @copyright  Arne Blankerts <arne@blankerts.de>, All rights reserved.
  * @license    BSD License
  */
-
 namespace TheSeer\phpDox\Generator\Engine {
 
     class HtmlConfig extends \TheSeer\phpDox\BuildConfig {
 
         public function getTemplateDirectory() {
             $default = $this->getGeneratorConfig()->getProjectConfig()->getHomeDirectory()->getPathname();
-            $default .=  '/templates/html';
+            $default .= '/templates/html';
             $node = $this->ctx->queryOne('cfg:template');
             if (!$node) {
                 return $default;
@@ -53,12 +52,12 @@ namespace TheSeer\phpDox\Generator\Engine {
         }
 
         public function getResourceDirectory() {
-          $default = $this->getTemplateDirectory().'/static';
-          $node = $this->ctx->queryOne('cfg:resource');
-          if (!$node) {
-              return $default;
-          }
-          return $node->getAttribute('path', $default);
+            $default = $this->getTemplateDirectory() . '/static';
+            $node = $this->ctx->queryOne('cfg:resource');
+            if (!$node) {
+                return $default;
+            }
+            return $node->getAttribute('path', $default);
         }
 
         public function getFileExtension() {
