37a30a188c4e83c33cd0c83dfda5708e065175f7
Arne Blankerts <Arne@Blankerts.de>
2015-06-15 22:10:15 +0200

    Update for php-timer 1.0.6 - fixes issue #235

--- src/vendor.php
+++ src/vendor.php
@@ -7,7 +7,6 @@ spl_autoload_register(
         static $classes = null;
         if ($classes === null) {
             $classes = array(
-                'php_timer' => '/phpunit/php-timer/PHP/Timer.php',
                 'phpparser\\autoloader' => '/nikic/php-parser/lib/PhpParser/Autoloader.php',
                 'phpparser\\builder' => '/nikic/php-parser/lib/PhpParser/Builder.php',
                 'phpparser\\builder\\class_' => '/nikic/php-parser/lib/PhpParser/Builder/Class_.php',
