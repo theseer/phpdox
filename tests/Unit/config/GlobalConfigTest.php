f52696f7c7fd81768d60322163c4a80e63482508
David Prévot <david@tilapin.org>
2016-03-17 22:11:04 -0400

    Use expectException instead of deprecated setExpectedException

--- tests/Unit/config/GlobalConfigTest.php
+++ tests/Unit/config/GlobalConfigTest.php
@@ -239,7 +239,7 @@ namespace TheSeer\phpDox {
          */
         public function testInvalidPropertyRequestThrowsException($file, $code) {
             $this->init('resolver/' .  $file);
-            $this->setExpectedException('TheSeer\\phpDox\\ConfigException', $code);
+            $this->expectException('TheSeer\\phpDox\\ConfigException', $code);
             $this->config->getProjects();
         }
 
