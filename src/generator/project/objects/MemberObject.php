f9c66a6666bdcc8d090083e74e11650fa264efd7
Arne Blankerts <Arne@Blankerts.de>
2016-08-31 14:04:35 +0200

    Fix #284

--- src/generator/project/objects/MemberObject.php
+++ src/generator/project/objects/MemberObject.php
@@ -3,7 +3,7 @@ namespace TheSeer\phpDox\Generator {
 
     use TheSeer\fDOM\fDOMElement;
 
-    class MemberObject {
+    class bMemberObject {
 
         /**
          * @var fDOMElement
@@ -19,15 +19,15 @@ namespace TheSeer\phpDox\Generator {
         }
 
         public function isPublic() {
-            return $this->node->getAttribute('visibility', 'public') == 'public';
+            return $this->node->getAttribute('visibility', 'public') === 'public';
         }
 
         public function isPrivate() {
-            return $this->node->getAttribute('visibility', 'public') == 'private';
+            return $this->node->getAttribute('visibility', 'private') === 'private';
         }
 
         public function isProtected() {
-            return $this->node->getAttribute('visibility', 'public') == 'protected';
+            return $this->node->getAttribute('visibility', 'protected') === 'protected';
         }
 
     }
