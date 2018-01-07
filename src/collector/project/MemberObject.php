eb803eb24ea7934070827acec3a05783c59234c1
Arne Blankerts <Arne@Blankerts.de>
2018-01-07 13:54:33 +0100

    Bump Copyright

--- src/collector/project/MemberObject.php
+++ src/collector/project/MemberObject.php
@@ -1,6 +1,6 @@
 <?php
     /**
-     * Copyright (c) 2010-2017 Arne Blankerts <arne@blankerts.de>
+     * Copyright (c) 2010-2018 Arne Blankerts <arne@blankerts.de>
      * All rights reserved.
      *
      * Redistribution and use in source and binary forms, with or without modification,
