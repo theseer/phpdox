1d6fe7b3990d367b4c4c63a0aa7bfe06889eada2
Cyril VERLOOP <cyril.verloop@webqard.fr>
2019-11-02 16:37:58 +0100

    Add Phan

---
+++ phan-config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'directory_list' => [
+        './src',
+        './vendor',
+    ],
+    'exclude_analysis_directory_list' => [
+        './vendor/'
+    ],
+    'minimum_severity' => 10,
+    'target_php_version' => '7.1'
+];
